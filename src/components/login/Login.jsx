bda64816fe86748e1bdd50d481ee20316b899932
Hojiakbar <hojiakbarrasulovich@gmail.com>
2023-03-03 14:28:05 +0500

    login and register page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Logo } from "../../constants";
+import { Input } from "../../ui";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>Login</div>
-  )
+    <div className="text-center mt-5">
+      <main className="form-signin w-25 m-auto">
+        <form>
+          <img className="mb-4" src={Logo} alt="" width="100" height="70" />
+          <h1 className="h3 mb-2 fw-normal">Please login</h1>
+
+          <Input label={"Email address"} state={email} setState={setEmail} />
+          <Input
+            label={"Password"}
+            type="password"
+            state={password}
+            setState={setPassword}
+          />
+
+          <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">
+            Login
+          </button>
+          <p className="mt-5 mb-3 text-muted">© 2017–2022</p>
+        </form>
+      </main>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Logo } from "../../constants";
+import { Input } from "../../ui";
 
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>Register</div>
-  )
+    <div className="text-center mt-5">
+      <main className="form-signin w-25 m-auto">
+        <form>
+          <img className="mb-4" src={Logo} alt="" width="100" height="70" />
+          <h1 className="h3 mb-2 fw-normal">Please register</h1>
+
+          <Input label={"UserName"} state={name} setState={setName} />
+          <Input label={"Email address"} state={email} setState={setEmail} />
+          <Input
+            label={"Password"}
+            type="password"
+            state={password}
+            setState={setPassword}
+          />
+
+          <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">
+            Register
+          </button>
+          <p className="mt-5 mb-3 text-muted">© 2017–2022</p>
+        </form>
+      </main>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
