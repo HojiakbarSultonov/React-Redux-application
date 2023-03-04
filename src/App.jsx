2c0fee6cdac16d5e48ad90879c4aac3f7bc3ef29
Hojiakbar <hojiakbarrasulovich@gmail.com>
2023-03-05 01:29:45 +0500

    Article card

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
     try {
       const response = await AuthService.getUser();
       dispatch(signUserSuccess(response.user));
-      console.log(response);
     } catch (error) {
       console.log(error);
     }
